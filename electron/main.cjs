e444201e927fb27354dd38b7e4bc43b060f6c9da
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2026-04-02 19:00:42 +0530

    changes

--- electron/main.cjs
+++ electron/main.cjs
@@ -26,7 +26,7 @@ function createWindow() {
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL('http://localhost:8080')
     // Uncomment to debug:
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
   } else {
     // In production load built files
     mainWindow.loadFile(
